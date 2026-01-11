464a98fafdd2ea3c9b8c492fa1325fd211c40ddf
arrowira <41136800+arrowira@users.noreply.github.com>
2026-01-10 23:41:49 -0800

    tracks to mouse

--- fractal/color_rect.gd
+++ fractal/color_rect.gd
@@ -1,5 +1,6 @@
 extends ColorRect
 
 func _physics_process(delta: float) -> void:
-	var t = get_local_mouse_position()
-	get_material().set_shader_parameter("target", t)
+	var t = get_viewport().get_mouse_position()
+	t/=get_viewport_rect().size
+	get_material().set_shader_parameter("focusPoint", t)
